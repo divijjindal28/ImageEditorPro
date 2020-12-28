8e2ac6b788d54164794b63f849401d0fed6c6ee4
Divij <divijjindal28@gmail.com>
2020-12-28 16:03:15 +0530

    run

--- lib/transform.dart
+++ lib/transform.dart
@@ -19,52 +19,41 @@ class TransformDemo extends StatelessWidget {
         height: 200,
         width: 200,
         color: Colors.blue,
-        child: Stack(
-          children: [
-            // image != null
-            //     ? Image.file(
-            //   image,
-            //
-            //   fit: BoxFit.cover,
-            // )
-            //     : Container(
-            //   height: 200,
-            //   width: 200,
-            //   color: Colors.blue,
-            // ),
-            MatrixGestureDetector(
-              onMatrixUpdate: (m, tm, sm, rm) {
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: FileImage(image)
+          )
+        ),
+        child: MatrixGestureDetector(
+          onMatrixUpdate: (m, tm, sm, rm) {
 
-                notifier.value = m;
-              },
-              shouldRotate: true,
-              shouldScale: true,
-              shouldTranslate: true,
-              child: AnimatedBuilder(
-                animation: notifier,
-                builder: (ctx, child) {
-                  print("animating ");
-                  return Transform(
+            notifier.value = m;
+          },
+          shouldRotate: true,
+          shouldScale: true,
+          shouldTranslate: true,
+          child: AnimatedBuilder(
+            animation: notifier,
+            builder: (ctx, child) {
+              print("animating ");
+              return Transform(
+                transform: notifier.value,
+                child:
+                Positioned.fill(
+                  child: Container(
                     transform: notifier.value,
-                    child:
-                    Positioned.fill(
-                      child: Container(
-                        transform: notifier.value,
-                        color: Colors.red,
-                        child: FittedBox(
-                          fit: BoxFit.contain,
-                          child: Text("hello",),
-                        ),
-                      ),
+                    color: Colors.red,
+                    child: FittedBox(
+                      fit: BoxFit.contain,
+                      child: Text("hello",),
                     ),
+                  ),
+                ),
 
 
-                  );
-                },
-              ),
-            ),
-          ],
-
+              );
+            },
+          ),
         ),
       ),
     );
