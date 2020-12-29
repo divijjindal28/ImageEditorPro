5d959a2885a93a845a7f8ca802ecbe497bf31c6e
Divij <divijjindal28@gmail.com>
2020-12-29 11:19:14 +0530

    run

--- lib/imageEditorPro2.dart
+++ lib/imageEditorPro2.dart
@@ -1,7 +1,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:image_editor_pro/transform.dart';
-class imageEditorPro2 extends StatelessWidget {
+class imageEditorPro2 extends StatefulWidget {
+  @override
+  _imageEditorPro2State createState() => _imageEditorPro2State();
+}
+
+class _imageEditorPro2State extends State<imageEditorPro2> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
