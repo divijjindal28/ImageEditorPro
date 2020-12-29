52cebbb054cc161ba7b2c5ea0df6076a092eac33
Divij <divijjindal28@gmail.com>
2020-12-29 10:45:28 +0530

    run

--- lib/transform.dart
+++ lib/transform.dart
@@ -4,41 +4,33 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:matrix_gesture_detector/matrix_gesture_detector.dart';
 
-class TransformDemo extends StatefulWidget {
-  final ValueNotifier<Matrix4> notifier = ValueNotifier(Matrix4.identity());
+class TransformDemo extends StatelessWidget {
 
   TransformDemo();
-
-  @override
-  _TransformDemoState createState() => _TransformDemoState();
-}
-
-class _TransformDemoState extends State<TransformDemo> {
   @override
   Widget build(BuildContext context) {
+    final ValueNotifier<Matrix4> notifier = ValueNotifier(Matrix4.identity());
     return MatrixGestureDetector(
 
           onMatrixUpdate: (m, tm, sm, rm) {
 
-            setState(() {
-              widget.notifier.value = m;
-
-            });
+            notifier.value = m;
           },
           shouldRotate: true,
           shouldScale: true,
           shouldTranslate: true,
           child: AnimatedBuilder(
-            animation: widget.notifier,
+            animation: notifier,
             builder: (ctx, child) {
               return Transform(
-                transform: widget.notifier.value,
+                transform: notifier.value,
+                transformHitTests: true,
                 child:
                 Stack(
                   children: [
                     Positioned.fill(
                       child: Container(
-                        transform: widget.notifier.value,
+                        transform: notifier.value,
                         color: Colors.blue,
                         child: FittedBox(
                           fit: BoxFit.contain,
